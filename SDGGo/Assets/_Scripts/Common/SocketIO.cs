4f2edb8982cb62ddb5bee974b2c2d3836a8afad1
SH-JIANGXH01-01\Administrator <jiangxinhou01@shandagames.com>
2017-09-18 21:08:22 +0800

    socketIO;

---
+++ SDGGo/Assets/_Scripts/Common/SocketIO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quobject.SocketIoClientDotNet.Client;
+using Newtonsoft.Json;
+
+public class SocketIO : Singleton<SocketIO> {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
